5dabcb5323e5c6a8bf964f15530bac7768ef1f41
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-29 15:01:27 +0100

    create the Response with details class

--- src/main/java/com/example/security/common/responses/ResponseWithDetails.java
+++ src/main/java/com/example/security/common/responses/ResponseWithDetails.java
@@ -1,4 +1,25 @@
 package com.example.security.common.responses;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Component
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseWithDetails {
+
+    private LocalDateTime timesTamps;
+    private String message;
+    private String status;
+    private Map<String,Object> details;
+
 }
