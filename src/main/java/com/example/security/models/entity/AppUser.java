8b9920230f9bdc80c24bbbaaac0c23cf44b81c29
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-30 14:26:37 +0100

    create app user class

---
+++ src/main/java/com/example/security/models/entity/AppUser.java
@@ -0,0 +1,55 @@
+package com.example.security.models.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AppUser implements UserDetails {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    private UUID id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+
+    private String password;
+
+    @ManyToMany(mappedBy = "users",fetch = FetchType.EAGER)
+    private Set<AppRole> roles = new HashSet<>();
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
