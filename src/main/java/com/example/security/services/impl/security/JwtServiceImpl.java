3d57de168b9dd0efb4c78e36c32b6c3ab85a0667
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-30 16:43:39 +0100

    create all methods to extract username from token

--- src/main/java/com/example/security/services/impl/security/JwtServiceImpl.java
+++ src/main/java/com/example/security/services/impl/security/JwtServiceImpl.java
@@ -1,19 +1,38 @@
 package com.example.security.services.impl.security;
 
 import com.example.security.services.facades.security.JwtService;
-import lombok.RequiredArgsConstructor;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Claims;
 
+import java.security.Key;
 import java.util.function.Function;
 
 @Service
 public class JwtServiceImpl implements JwtService {
+
+    @Value("${token.signing.key}")
+    private String jwtSigningKey;
     @Override
     public String extractUserName(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
     }
-
+    private <T> T extractClaim(String token, Function<Claims,T> claimsResolvers){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolvers.apply(claims);
+    }
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token)
+                .getBody();
+    }
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSigningKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
     @Override
     public String generateAccessToken(UserDetails userDetails) {
         return null;
@@ -29,8 +48,5 @@ public class JwtServiceImpl implements JwtService {
         return false;
     }
 
-    private <T> T extractClaim(String token, Function<Claims,T> claimsResolvers){
-        final Claims claims =extractAllClaims(token);
-        return claimsResolvers.apply(claims);
-    }
+
 }
