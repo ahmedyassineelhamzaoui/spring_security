38fd47b23076d03b0c180127ba9ef2012290e89d
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-30 16:24:34 +0100

    create authentication service implmentation

---
+++ src/main/java/com/example/security/services/impl/security/AuthenticationServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.security.services.impl.security;
+
+import com.example.security.services.facades.security.AuthenticationService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationServiceImpl  implements AuthenticationService {
+}
