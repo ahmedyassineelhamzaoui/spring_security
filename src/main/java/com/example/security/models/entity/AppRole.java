abc399634de3ab132d0b6795ae16c27807f03dbf
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-30 14:31:22 +0100

    create app role class

---
+++ src/main/java/com/example/security/models/entity/AppRole.java
@@ -0,0 +1,27 @@
+package com.example.security.models.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppRole {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    private UUID id;
+
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles",fetch = FetchType.EAGER)
+    private Set<AppPermission> permissions = new HashSet<>();
+
+}
