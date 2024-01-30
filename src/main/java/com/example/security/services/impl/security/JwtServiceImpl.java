7f64cd08d4aa18ecd1a99a8cf70e2446a488c879
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-30 16:38:33 +0100

    implemant all methods

--- src/main/java/com/example/security/services/impl/security/JwtServiceImpl.java
+++ src/main/java/com/example/security/services/impl/security/JwtServiceImpl.java
@@ -2,8 +2,35 @@ package com.example.security.services.impl.security;
 
 import com.example.security.services.facades.security.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
 
 @Service
 public class JwtServiceImpl implements JwtService {
+    @Override
+    public String extractUserName(String token) {
+        return null;
+    }
+
+    @Override
+    public String generateAccessToken(UserDetails userDetails) {
+        return null;
+    }
+
+    @Override
+    public String generateRefreshToken(UserDetails userDetails) {
+        return null;
+    }
+
+    @Override
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        return false;
+    }
+
+    private <T> T extractClaim(String token, Function<Claims,T> claimsResolvers){
+        final Claims claims =extractAllClaims(token);
+        return claimsResolvers.apply(claims);
+    }
 }
