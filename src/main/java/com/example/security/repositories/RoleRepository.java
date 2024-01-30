0516614e08cc3da5770706035c056156133ccb38
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-30 16:05:20 +0100

    create role repository

---
+++ src/main/java/com/example/security/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.example.security.repositories;
+
+import com.example.security.models.entity.AppRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RoleRepository extends JpaRepository<AppRole, UUID> {
+}
