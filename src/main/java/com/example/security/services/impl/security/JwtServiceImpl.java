bc59b9869cb5e2d5dfdddf0eb14fd00e0d016075
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-30 16:23:33 +0100

    create jwt service implmentation

---
+++ src/main/java/com/example/security/services/impl/security/JwtServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.security.services.impl.security;
+
+import com.example.security.services.facades.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+}
