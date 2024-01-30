8c4a2bab295ea0e2e3d247c7b79dff17875ddf67
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-30 16:26:31 +0100

    create user serice implmentation

---
+++ src/main/java/com/example/security/services/impl/security/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.security.services.impl.security;
+
+import com.example.security.services.facades.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
