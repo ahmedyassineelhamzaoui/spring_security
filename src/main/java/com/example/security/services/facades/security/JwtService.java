36f1de5c2b99f9d7681c44a4edfe6088816ff2ff
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-30 16:31:38 +0100

    create all necessary method in jwt service interface

--- src/main/java/com/example/security/services/facades/security/JwtService.java
+++ src/main/java/com/example/security/services/facades/security/JwtService.java
@@ -1,4 +1,14 @@
 package com.example.security.services.facades.security;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface JwtService {
+
+    String extractUserName(String token);
+
+    String generateAccessToken(UserDetails userDetails);
+
+    String generateRefreshToken(UserDetails userDetails);
+
+    boolean isTokenValid(String token,UserDetails userDetails);
 }
--- src/main/java/com/example/security/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/security/services/impl/UserServiceImpl.java
@@ -1,4 +1,4 @@
-package com.example.security.services.impl.security;
+package com.example.security.services.impl;
 
 import com.example.security.services.facades.UserService;
 import org.springframework.stereotype.Service;
