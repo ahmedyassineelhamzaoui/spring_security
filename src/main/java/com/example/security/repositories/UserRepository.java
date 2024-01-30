9024ddaacd5dda886c9c042071752ee1eeb87fd6
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-30 16:03:28 +0100

    create user repository

---
+++ src/main/java/com/example/security/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.security.repositories;
+
+import com.example.security.models.entity.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<AppUser, UUID> {
+}
