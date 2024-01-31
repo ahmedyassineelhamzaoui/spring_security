84662586bbb97fd11db43b6f72fd22f7360eb2ae
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-31 09:28:45 +0100

    create signup request

---
+++ src/main/java/com/example/security/models/dto/request/SignupRequest.java
@@ -0,0 +1,28 @@
+package com.example.security.models.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignupRequest {
+
+    @NotBlank(message = "First name is required")
+    @Size(min=2,max=20,message="First name must be between 2 and 20 characters")
+    private String firstName;
+
+    @NotBlank(message ="Last name is required")
+    @Size(min=2,max=20,message="Last name must be between 2 and 20 characters")
+    private String lastName;
+
+    @NotBlank(message ="username is required")
+    private String username;
+
+    @NotBlank(message ="password is required")
+    @Size(min=8,max=30,message = "Password must be between  6 and 30 characters")
+    @Pattern(regexp ="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$",message ="password must be min 8 and max 30 length containing at least 1 uppercase, 1 lowercase, 1 special character and 1 digit")
+    private String password;
+}
