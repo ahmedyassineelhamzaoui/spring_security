e5ba8ce9c799c393790fa6ea062f818dfaaa7e28
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-31 08:36:51 +0100

    add all necessary feilds in authentication service class

--- src/main/java/com/example/security/services/impl/security/AuthenticationServiceImpl.java
+++ src/main/java/com/example/security/services/impl/security/AuthenticationServiceImpl.java
@@ -1,8 +1,19 @@
 package com.example.security.services.impl.security;
 
+import com.example.security.repositories.UserRepository;
 import com.example.security.services.facades.security.AuthenticationService;
+import com.example.security.services.facades.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthenticationServiceImpl  implements AuthenticationService {
+       private final UserRepository userRepository;
+       private final PasswordEncoder passwordEncoder;
+       private final JwtService jwtService;
+       private final AuthenticationManager authenticationManager;
+
 }
--- src/main/java/com/example/security/services/impl/security/JwtServiceImpl.java
+++ src/main/java/com/example/security/services/impl/security/JwtServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.security.services.impl.security;
 
 import com.example.security.services.facades.security.JwtService;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,7 +11,11 @@ import org.springframework.stereotype.Service;
 import io.jsonwebtoken.Claims;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class JwtServiceImpl implements JwtService {
@@ -35,17 +40,41 @@ public class JwtServiceImpl implements JwtService {
     }
     @Override
     public String generateAccessToken(UserDetails userDetails) {
-        return null;
+
+        Map<String,Object> roles= new HashMap<>();
+        Map<String,Object> permissions = new HashMap<>();
+        Map<String,Object> extraClaims= new HashMap<>();
+        roles.put("roles",userDetails.getAuthorities().stream().map(role -> role.getAuthority()).collect(Collectors.toList()));
+        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .addClaims(roles)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
+
     }
 
     @Override
     public String generateRefreshToken(UserDetails userDetails) {
-        return null;
+
+        Map<String,Object> extraClaims= new HashMap<>();
+        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 24 * 60 *100))
+                .signWith(getSigningKey(),SignatureAlgorithm.HS256).compact();
+
     }
 
     @Override
     public boolean isTokenValid(String token, UserDetails userDetails) {
-        return false;
+        final String userName = extractUserName(token);
+        return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
 
