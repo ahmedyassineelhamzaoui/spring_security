d43dca21d2966fda764f90b4aaa4bf463f9d6132
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-31 08:54:20 +0100

    create method do filterinternal

--- src/main/java/com/example/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/security/filter/JwtAuthenticationFilter.java
@@ -1,15 +1,19 @@
 package com.example.security.filter;
 
+import com.example.security.services.facades.UserService;
+import com.example.security.services.facades.security.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.constraints.NotNull;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -19,9 +23,34 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private final JwtService jwtService;
+    private final UserService userService;
 
     @Override
-    protected void doFilterInternal(@NotNull HttpServletRequest request,@NotNull HttpServletResponse response,@NotNull FilterChain filterChain) throws ServletException, IOException {
-
+    protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String username;
+        if(StringUtils.isEmpty(authHeader) || !StringUtils.startsWith(authHeader,"Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        username =jwtService.extractUserName(jwt);
+        if(StringUtils.isNotEmpty(username) && SecurityContextHolder.getContext().getAuthentication() == null){
+            try{
+                UserDetails userDetails =userService.userDetailsService().loadUserByUsername(username);
+                if(jwtService.isTokenValid(jwt,userDetails)){
+                    SecurityContext context =SecurityContextHolder.createEmptyContext();
+                    UsernamePasswordAuthenticationToken authToken =new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    context.setAuthentication(authToken);
+                    SecurityContextHolder.setContext(context);
+                    filterChain.doFilter(request,response);
+                }
+            }catch(Exception e){
+                System.out.println("!!error message!! : "+e.getMessage());
+            }
+        }
     }
 }
