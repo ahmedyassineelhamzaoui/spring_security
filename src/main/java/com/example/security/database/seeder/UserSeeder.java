ec909d4d94c90ec80307f7485b89d89dbfed0731
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-01-31 11:51:21 +0100

    create method assign role to users

--- src/main/java/com/example/security/database/seeder/UserSeeder.java
+++ src/main/java/com/example/security/database/seeder/UserSeeder.java
@@ -1,13 +1,16 @@
 package com.example.security.database.seeder;
 
 import com.example.security.models.entity.AppUser;
+import com.example.security.repositories.RoleRepository;
 import com.example.security.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Component
 @RequiredArgsConstructor
@@ -15,6 +18,7 @@ public class UserSeeder {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     public void seed(){
         if(userRepository.count() == 0){
@@ -61,6 +65,38 @@ public class UserSeeder {
                     .build()
             );
             userRepository.saveAll(users);
+            assignRolesToUsers();
         }
+    }
+    private void assignRolesToUsers(){
+        AppUser user1 = userRepository.findByUsername("john123").orElseThrow(() -> new UsernameNotFoundException("username not found"));
+        user1.setAuthorities(roleRepository.findAll());
+        userRepository.save(user1);
+
+        AppUser user2 = userRepository.findByUsername("jane456").orElseThrow(() -> new UsernameNotFoundException("username not found"));
+        user1.setAuthorities(List.of(
+                roleRepository.findByName("ADMIN").orElseThrow(() -> new NoSuchElementException("role not found")),
+                roleRepository.findByName("USER").orElseThrow(() -> new NoSuchElementException("role not found"))
+        ));
+        userRepository.save(user2);
+
+        AppUser user3 = userRepository.findByUsername("mike789").orElseThrow(() -> new UsernameNotFoundException("username not found"));
+        user3.setAuthorities(List.of(
+                roleRepository.findByName("ADMIN").orElseThrow(() -> new NoSuchElementException("role not found")),
+                roleRepository.findByName("USER").orElseThrow(() -> new NoSuchElementException("role not found"))
+        ));
+        userRepository.save(user3);
+
+        AppUser user4 = userRepository.findByUsername("sarah1011").orElseThrow(() -> new UsernameNotFoundException("username not found"));
+        user4.setAuthorities(List.of(
+                roleRepository.findByName("ADMIN").orElseThrow(() -> new NoSuchElementException("role not found"))
+        ));
+        userRepository.save(user4);
+
+        AppUser user5 = userRepository.findByUsername("rob1213").orElseThrow(() -> new UsernameNotFoundException("username not found"));
+        user5.setAuthorities(List.of(
+                roleRepository.findByName("USER").orElseThrow(() -> new NoSuchElementException("role not found"))
+        ));
+        userRepository.save(user5);
     }
 }
